c0780ff3fc26a75ee7311a155d2a863d4c050f85
Oliver Smith <smith_oliver@live.com>
2019-11-22 18:17:06 +0000

    started making models

--- E_Learning/public/views.py
+++ E_Learning/public/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
-from .forms import LoginForm
+from common.forms import LoginForm
 
 
 # Create your views here.
