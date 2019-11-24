895c9abf4a9f67b7afbf4b230d7e47d6191f46b3
Oliver Smith <smith_oliver@live.com>
2019-11-24 20:21:35 +0000

    updated views to import models correctly

--- E_Learning/manager/views.py
+++ E_Learning/manager/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponse
 
-from E_Learning.common.models import AppUsers
+from common.models import AppUsers
 
 
 def manager_check(user):
-    return user.users.account_type == AppUsers.MANAGER
+    if user.is_anonymous:
+        return False
+    else:
+        return user.AppUsers.account_type == AppUsers.MANAGER
 
 
 @user_passes_test(manager_check)  # TODO: Add decorator to all views
