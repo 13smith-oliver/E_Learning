2ea98ec87716698435858f16404025ca834703d8
Oliver Smith <smith_oliver@live.com>
2019-11-25 22:10:38 +0000

    small changes to views and login html, overhaul of login css

--- E_Learning/corporate/views.py
+++ E_Learning/corporate/views.py
@@ -1,10 +1,9 @@
+from common.models import AppUsers
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
-from common.models import AppUsers
 
-
-def corporate_check(user): # TODO: Add check as function to all views
+def corporate_check(user):  # TODO: Add check as function to all views
     if user.is_anonymous or user.AppUsers.account_type != AppUsers.CORPORATE:
         return redirect('http://osmith.me/login/corporate')
 
--- E_Learning/manager/views.py
+++ E_Learning/manager/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 from common.models import AppUsers
 
 
-def manager_check(user): # TODO: Add check as function to all views
+def manager_check(user):  # TODO: Add check as function to all views
     if user.is_anonymous or user.AppUsers.account_type != AppUsers.MANAGER:
         return redirect('http://osmith.me/login/manager')
-        
+
 
 def manager_home(request):
     check = manager_check(request.user)
