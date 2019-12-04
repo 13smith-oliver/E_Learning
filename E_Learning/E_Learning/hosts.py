cac650a5bdd0a67e608b030b1af9da075934e7e1
Oliver Smith <smith_oliver@live.com>
2019-12-04 22:26:53 +0000

    add comments to hosts file

--- E_Learning/E_Learning/hosts.py
+++ E_Learning/E_Learning/hosts.py
@@ -1,6 +1,9 @@
+# E_Learning/Hosts.py
+
 from django.conf import settings
 from django_hosts import patterns, host
 
+# Defines the host patterns which will load the corresponding urls file for the subdomain that the user is connecting to
 host_patterns = patterns('',
                          host(r'manager', 'manager.urls', name='manager_urls'),
                          host(r'corporate', 'corporate.urls', name='corporate_urls'),
